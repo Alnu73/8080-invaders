32ad302e0f582552a561960f94de3a5f8e4a7ec8
Alnu73 <axel8744@gmail.com>
2023-07-31 11:00:05 +0200

    Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <fstream>
+
 int main(int argc, char** argv) {
+    std::fstream rfile;
+    rfile.open("./invaders/invaders", std::ios::out);
+    if (!rfile) {
+        std::cout<<"Error opening file. Aborting.\n";
+    }
+    else {
+        std::cout<<"File successfully opened.\n";
+        rfile.close();
+    }
     return 0;
 }
